193cbaa3f099089fd12801cf92c500a8cd578bf8
Henri Tarkiainen <henri.tarkiainen@gmail.com>
2021-04-09 00:36:04 +0300

    routes login

--- routes.py
+++ routes.py
@@ -6,17 +6,14 @@ import users
 def index():
     return render_template("index.html")
 
-@app.route("/login", methods=["GET","POST"])
+@app.route("/login", methods=["POST"])
 def login():
-    if request.method == "GET":
-        return render_template("login.html")
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-        if users.login(username,password):
-            return redirect("/")
-        else:
-            return render_template("error.html",message="Väärä tunnus tai salasana")
+    username = request.form["username"]
+    password = request.form["password"]
+    if users.login(username,password):
+        return redirect("/")
+    else:
+        return render_template("error.html",message="Väärä tunnus tai salasana")
 
 @app.route("/logout")
 def logout():
